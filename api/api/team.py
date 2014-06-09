8ba0df75ed2fc54b23e7bed171ce56a8947c68f5
Jonathan Burket <jburket@cmu.edu>
2014-06-08 20:33:40 -0400

    Added new page/js for a 'team' page

--- api/api/team.py
+++ api/api/team.py
@@ -2,7 +2,7 @@ __author__ = 'collinpetty'
 
 from api.annotations import *
 from api import app, common
-
+import api.user
 
 def get_team(tid=None, team_name=None):
     db = common.get_conn()
@@ -28,3 +28,30 @@ def create_team(team_name, adviser_name, adviser_email, school, password):
         print("Error creating the team account.")
         return None
     return tid
+
+
+def get_teammate_uids(tid):
+    db = common.get_conn()
+    return [t['uid'] for t in db.users.find({'tid': tid})]
+
+
+@app.route('/api/team', methods=['GET'])
+@return_json
+@require_login
+def team_information():
+    db = common.get_conn()
+    useracct = api.user.get_user()
+    uid = useracct['uid']
+    tid = useracct['tid']
+    team_cur = db.teams.find_one({'tid': tid})
+
+    teammates = []
+    for u in get_teammate_uids(tid):
+        teammate = db.users.find_one({'uid': u})
+        teammates.append({'teammate': teammate['username'], 'is_you': teammate['uid'] == uid})
+
+    team_data = {'team_name': team_cur['team_name'], 'password': team_cur['password'],
+                 'adviser_name': team_cur['adviser_name'], 'adviser_email': team_cur['adviser_email'],
+                 'school': team_cur['school'],
+                 'teammates': teammates}
+    return 1, team_data
