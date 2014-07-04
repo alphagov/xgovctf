9bc6c34bf20613e2c29b1bec7261a2839b1f0dfa
cganas <ganaschristopher@gmail.com>
2014-07-04 17:58:03 -0400

    Added update_password test.

--- api/tests/user_test.py
+++ api/tests/user_test.py
@@ -5,7 +5,9 @@ Testing Module
 import pytest
 import api.user
 import api.common
+import bcrypt
 
+from api.common import APIException
 from common import clear_collections
 
 @pytest.mark.usefixtures("db")
@@ -13,6 +15,7 @@ class TestUsers(object):
     """
     API Tests for User.py
     """
+
     @clear_collections("user")
     def test_create_batch_normal_users(self, users=100):
         """
@@ -28,7 +31,7 @@ class TestUsers(object):
         for i in range(users):
             name = "fred" + str(i)
             uids.append(api.user.create_user(
-                name, name + "@gmail.com", "HASH"
+                name, name + "@gmail.com", name
             ))
 
         assert len(set(uids)) == len(uids), "UIDs are not unique."
@@ -44,10 +47,40 @@ class TestUsers(object):
 
             assert user_from_uid == user_from_name, "User lookup from uid and name are not the same."
 
+    #TODO: Team should be tested first
+    @clear_collections("user", "team")
     def test_register_user(self, db):
         """
         Tests the registration and form validation functionality.
         """
-
         pass
+
+    @clear_collections("user")
+    def test_change_pw_user(self):
+        """
+        Tests password change functionality.
+
+        Covers:
+            user.update_password
+            user.hash_password
+        Depends:
+            user.get_user
+        """
+        uid = api.user.create_user("fred", "fred@gmail.com", "HASH")
+
+        old_hash = api.user.get_user(uid=uid)["pwhash"]
+        assert old_hash == "HASH", "Was unable to confirm password was stored correctly."
+
+        api.user.update_password(uid, "HACK")
+
+        new_hash = api.user.get_user(uid=uid)["pwhash"]
+
+        assert bcrypt.hashpw("HACK", new_hash) == new_hash, \
+            "Password does not match hashed plaintext after changing it."
+
+        with pytest.raises(APIException):
+            api.user.update_password(uid, "")
+            assert False, "Should not be able to update password to nothing."
+
+
 
