af6a563c6cab5e8f63eca1472477da55dabba12e
cganas <ganaschristopher@gmail.com>
2014-07-04 17:18:22 -0400

    Added clear_collections decorator.

---
+++ api/tests/common.py
@@ -0,0 +1,22 @@
+import pytest
+import api.common
+
+from functools import wraps
+
+def clear_collections(collections):
+    """
+    Clears collections listed after function has completed.
+    """
+    def clear(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            result = f(*args, **kwargs)
+
+            db = api.common.get_conn()
+            for collection in collections:
+                db[collection].remove()
+                assert len(list(db[collection].find())) == 0, "Unable to clear collection: " + collection
+
+            return result
+        return wrapper
+    return clear
