4a5632a9741ba650711080e437387269471e996f
Tim Becker <tjbecker512@gmail.com>
2014-07-07 08:06:45 -0400

    Added more test code for team functionality.

--- api/tests/team_test.py
+++ api/tests/team_test.py
@@ -5,6 +5,7 @@ Team Testing Module
 
 import pytest
 import api.user
+import api.team
 import api.common
 import bcrypt
 
@@ -24,8 +25,18 @@ class TestTeams(object):
         "school": "Test HS",
         "password": "much_protected"
     }
+
+    base_user = {
+        "username": "valid",
+        "pass": "valid",
+        "email": "valid@hs.edu",
+        "create-new-team": "false",
+
+        "team-name-existing": base_team['team_name'],
+        "team-pass-existing": base_team['password']
+    }
     
-    @clear_collections("team")
+    @clear_collections("teams")
     def test_create_batch_teams(self, teams=10):
         """
         Tests team creation.
@@ -46,11 +57,54 @@ class TestTeams(object):
         assert len(api.team.get_all_teams()) == len(tids), "Not all teams were created."
 
         for i, tid in enumerate(tids):
-            name = "team" + str(i)
+            name = self.base_team['team_name'] + str(i)
 
             team_from_tid = api.team.get_team(tid=tid)
             team_from_name = api.team.get_team(name=name)
 
-            assert team_from_tid == team_from_name, "team lookup from tid and name are not the same."
+            assert team_from_tid == team_from_name, "Team lookup from tid and name are not the same."
 
+    @clear_collections("teams", "users")
+    def test_get_team_uids(self):
+        """
+        Tests the code that retrieves the list of uids on a team
 
+        Covers:
+            team.create_team
+            user.register_user
+            team.get_team_uids
+        """
+
+        tid = api.team.create_team(self.base_team.copy())
+        
+
+        uids = []
+        for i in range(api.team.max_team_users):
+            test_user = self.base_user.copy()
+            test_user['username'] += str(i)
+            uids.append(api.user.register_user(test_user))
+        
+        team_uids = api.team.get_team_uids(tid)
+        assert len(team_uids) == api.team.max_team_users, "Team does not have correct number of members"
+        assert sorted(uids) == sorted(team_uids), "Team does not have the correct members"
+
+    @clear_collections("teams", "users")
+    def test_team_size_validation(self):
+        """
+        Tests the team size restriction
+
+        Covers:
+            team.create_team
+            user.register_user
+        """
+
+        api.team.create_team(self.base_team.copy())
+        
+        for i in range(api.team.max_team_users):
+            test_user = self.base_user.copy()
+            test_user['username'] += str(i)
+            api.user.register_user(test_user)
+
+        with pytest.raises(APIException):
+            api.user.register_user(self.base_user.copy())
+            assert False, "Team has too many users"
