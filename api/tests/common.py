f31a5e65aee4dfec95c56077e5dacc5dc21c05f9
Chris Ganas <cganas@cmu.edu>
2014-07-07 10:30:52 -0500

    Updated clear_collections decorator.

--- api/tests/common.py
+++ api/tests/common.py
@@ -6,20 +6,23 @@ from functools import wraps
 def clear_collections(*collections):
     """
     Clears collections listed after function has completed.
+    Will throw an assertion if any collection is not empty when called.
     """
     def clear(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
+            db = api.common.get_conn()
+            collection_size = lambda name: len(list(db[name].find()))
+            for collection in collections:
+                assert collection_size(collection) == 0, "Collection was not empty: " + collection
             try:
                 result = f(*args, **kwargs)
             except Exception:
                 raise
             finally:
-                db = api.common.get_conn()
+                #Clear the collections.
                 for collection in collections:
                     db[collection].remove()
-                    assert len(list(db[collection].find())) == 0, "Unable to clear collection: " + collection
-
             return result
         return wrapper
     return clear
