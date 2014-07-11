755071fd6ce70a11fa894219b8643e720c093039
Tim Becker <tjbecker512@gmail.com>
2014-07-11 11:34:27 -0400

    Unnecessary imports

--- api/api/__init__.py
+++ api/api/__init__.py
@@ -1,6 +1,1 @@
-from flask import Flask, url_for, request, session
-from api import setup, user, auth, team, problem, scoreboard, utilities, game
-from api.common import APIException
-from api.annotations import return_json, require_login, require_admin, log_request
-
-import api.common
+import api
