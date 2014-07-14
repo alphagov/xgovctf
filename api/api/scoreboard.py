ca57a349077151c32b2ec6d13dac6318acbddb8d
cganas <ganaschristopher@gmail.com>
2014-07-13 21:34:47 -0400

    Removed old caching solution.

--- api/api/scoreboard.py
+++ api/api/scoreboard.py
@@ -11,6 +11,7 @@ def get_score(tid=None, uid=None):
     """
     Get the score for a user or team.
     Looks for a cached score, if not found we query all correct submissions by the team and add up their scores if they exist. Cache the result.
+
     Args:
         tid: The team id
         uid: The user id
@@ -18,21 +19,7 @@ def get_score(tid=None, uid=None):
         The users's or team's score
     """
 
-    db = api.common.get_conn()
-
-    if tid is not None:
-        cache_name = "teamscore_" + tid
-    elif uid is not None:
-        cache_name = "userscore_" + uid
-    else:
-        raise APIException(0, None, "Must supply uid or tid")
-
-    score = cache.get(cache_name)
-    if score is not None:
-        return score
-
     pids = [s['pid'] for s in api.problem.get_correct_submissions(tid=tid, uid=uid)]
     score = sum([api.problem.get_problem(pid)['score'] for pid in pids])
 
-    cache.set(cache_name, score, 60 * 60)
     return score
