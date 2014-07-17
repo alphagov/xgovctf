2f791ab72b8ee842457d486f94a84cd6118b7716
Chris Ganas <cganas@cmu.edu>
2014-07-17 15:00:15 -0500

    Statistics handler.

--- api/api/logger.py
+++ api/api/logger.py
@@ -5,17 +5,72 @@ Manage loggers for the api.
 import logging
 import api
 
+from bson import json_util
+
 from flask import request, has_request_context
 from flask import logging as flask_logging
 
 from sys import stdout
-from colorama import init, Fore, Style
-
-init(autoreset=True)
+from datetime import datetime
 
 log = logging.getLogger(__name__)
 
-seperator = "-" * 80
+#TODO: Add configuration for this.
+class StatsHandler(logging.StreamHandler):
+    """
+    Logs statistical information into the mongodb.
+    """
+
+    time_format = "%H:%M:%S %Y-%m-%d"
+
+    action_parsers = {
+        "api.user.create_user_request":
+            lambda params, result=None: {
+                "username": params["username"],
+                "new_team": params["create-new-team"] == "on"
+            }
+    }
+
+    def __init__(self):
+
+        self.db = api.common.get_conn()
+        logging.StreamHandler.__init__(self)
+
+    def emit(self, record):
+        """
+        Store record into the db.
+        """
+
+        information = get_request_information()
+
+        result = record.msg
+
+        if type(result) == dict:
+
+            information.update({
+                "event": result["name"],
+                "time": datetime.now().strftime(self.time_format)
+            })
+
+            information["action"] = {
+                "pass": True
+            }
+
+            if "exception" in result:
+                information["action"].update({
+                    "exception": result["exception"],
+                    "pass": False
+                })
+            elif result["name"] in self.action_parsers:
+                action_parser = self.action_parsers[result["name"]]
+
+                result["kwargs"]["result"] = result["result"]
+                action_result = action_parser(*result["args"], **result["kwargs"])
+
+                information["action"].update(action_result)
+
+            print(json_util.dumps(information, indent=4))
+            self.db.statistics.insert(information)
 
 def set_level(name, level):
     """
@@ -42,67 +97,42 @@ def use(name):
 
     return logging.getLogger(name)
 
-def _flask_request_format(msg):
+def get_request_information():
     """
-    Wraps a message with the flask request context if available.
+    Returns a dictionary of contextual information about the user at the time of logging.
 
-    Args:
-        msg: The message to wrap
     Returns:
-        The wrapped message
+        The dictionary.
     """
 
-    log_base = "{}\n"
+    information = {}
 
     if has_request_context():
-        flask_log_base = "Request: {method} {path}\nIP: {ip} Agent: {agent_platform} | {agent_browser} {agent_browser_version}" \
-        .format(
-            method=request.method,
-            path=request.path,
-            ip=request.remote_addr,
-            agent_platform=request.user_agent.platform,
-            agent_browser=request.user_agent.browser,
-            agent_browser_version=request.user_agent.version,
-            agent=request.user_agent.string
-        )
-
-        log_base = log_base.format(flask_log_base) + "{}\n"
+        information["request"] = {
+            "api_endpoint_method": request.method,
+            "api_endpoint": request.path,
+            "ip": request.remote_addr,
+            "platform": request.user_agent.platform,
+            "browser": request.user_agent.browser,
+            "browser_version": request.user_agent.version,
+            "user_agent":request.user_agent.string
+        }
 
         if api.auth.is_logged_in():
+
             user = api.user.get_user()
-            team = api.user.get_team(user["uid"])
-            user_log_base = "User: {}  Email: {} Team: {}".format(user["username"], user["email"], team["team_name"])
-            log_base = log_base.format(user_log_base) + "{}\n"
+            team = api.user.get_team()
+            groups = api.team.get_groups()
 
-    return log_base.format(msg) + seperator
+            information["user"] = {
+                "username": user["username"],
+                "email": user["email"],
+                "team_name": team["team_name"],
+                "school": team["school"],
+                "groups": [group["name"] for group in groups]
+            }
 
-class ColorFormatter(logging.Formatter):
-    """
-    Visual logging!
-    """
-
-    colors = {
-        logging.INFO: (Fore.GREEN, Fore.WHITE),
-        logging.DEBUG: (Fore.MAGENTA, Fore.WHITE),
-        logging.WARNING: (Fore.YELLOW, Fore.YELLOW),
-        logging.ERROR: (Fore.RED, Fore.YELLOW),
-        logging.CRITICAL: (Fore.RED, Fore.RED)
-    }
-
-    style = logging._STYLES["%"][0] # pylint: disable=protected-access
-
-    def __init__(self, fmt="%(levelno)s: %(msg)s"):
-        logging.Formatter.__init__(self, fmt, "%H:%M", style="%")
-
-    def format(self, record):
-        color, text = self.colors.get(record.levelno, self.colors[logging.INFO])
-
-        log_text = '{3}%(asctime)s{2} %(name)-8s {4}{3}%(levelname)-8s{1}{5} {6}%(message)s{1}'.format(
-            Fore.WHITE, Fore.RESET, Fore.WHITE, color, Style.NORMAL, Style.RESET_ALL, text)
-
-        self._style = self.style(_flask_request_format(log_text))
-
-        return logging.Formatter.format(self, record)
+    return information
 
 def setup_logs(args):
     """
@@ -120,9 +150,17 @@ def setup_logs(args):
     level = [logging.WARNING, logging.INFO, logging.DEBUG][
         min(args.get("verbose", 1), 2)]
 
-    stdout_log = logging.StreamHandler(stdout)
-    stdout_log.setFormatter(ColorFormatter())
+    internal_error_log = logging.StreamHandler(stdout)
+    internal_error_log.setLevel(logging.ERROR)
 
-    log.root.addHandler(stdout_log)
+    severe_error_log = logging.StreamHandler(stdout)
+    severe_error_log.setLevel(logging.CRITICAL)
+
+    stats_log = StatsHandler()
+    stats_log.setLevel(logging.INFO)
+
     log.root.setLevel(level)
+    log.root.addHandler(internal_error_log)
+    log.root.addHandler(severe_error_log)
+    log.root.addHandler(stats_log)
 
