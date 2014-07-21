98d893713fbffe3013eb99ed72aa50cab6eb442c
cganas <ganaschristopher@gmail.com>
2014-07-21 04:08:15 -0400

    cache library draft, will add onto it as we need it.

--- api/api/cache.py
+++ api/api/cache.py
@@ -6,34 +6,26 @@ from functools import wraps
 from bson import json_util
 
 import api
-from pymemcache.client import Client
+import redis
 
 from api.common import InternalException
 
 log = api.logger.use(__name__)
 
-memcache_host = "127.0.0.1"
-memcache_port = 11211
+redis_host = "127.0.0.1"
+redis_port = 6379
 
-def json_serializer(_, value):
+no_cache = False
+
+cache = redis.StrictRedis(host=redis_host, port=redis_port, db=0)
+fast_cache = {}
+
+def clear():
     """
-    Memcache json serializer
+    Clears the cache.
     """
-
-    return (value, 1) if type(value) == str else (json_util.dumps(value), 2)
-
-def json_deserializer(_, value, flag):
-    """
-    Memcache json deserializer
-    """
-    return value if flag == 1 else json_util.loads(value.decode("utf-8"))
-
-cache = Client(
-    (memcache_host, memcache_port),
-    serializer=json_serializer,
-    deserializer=json_deserializer
-)
-
+    cache.flushdb()
+    fast_cache.clear()
 
 def get_key(f, *args, **kwargs):
     """
@@ -51,7 +43,41 @@ def get_key(f, *args, **kwargs):
 
     return key
 
-def memoize(timeout=0):
+def get(key, deserialize=True):
+    """
+    Get a key from the cache.
+
+    Args:
+        key: cache key
+        deserialize: Whether or not the data should be deserialized.
+    Returns:
+        The result deserialized.
+    """
+
+    cached_result = cache.get(key)
+
+    if deserialize and cached_result is not None:
+        cached_result = json_util.loads(cached_result.decode("utf-8"))
+
+    return cached_result
+
+def set(key, value, timeout=0, serialize=True):
+    """
+    Set a key in the cache.
+
+    Args:
+        key: The cache key.
+        timeout: Time the key is valid.
+        serialize: Whether or not the data should be serialized.
+    """
+
+    if serialize:
+        value = json_util.dumps(value)
+
+    cache.setex(key, timeout, value)
+
+
+def fast_memoize():
     """
     Cache a function based on its arguments.
 
@@ -74,12 +100,49 @@ def memoize(timeout=0):
 
             key = get_key(f, *args, **kwargs)
 
-            cached_result = cache.get(key)
+            cached_result = fast_cache.get(key)
 
-            if cached_result is None:
+            if cached_result is None or no_cache:
                 function_result = f(*args, **kwargs)
 
-                cache.set(key, function_result, timeout)
+                fast_cache[key] = function_result
+                return function_result
+
+            return cached_result
+
+        return wrapper
+
+    return decorator
+
+def memoize(timeout=0, serialize=True, deserialize=True):
+    """
+    Cache a function based on its arguments.
+
+    Args:
+        timout: Time the result stays valid in the cache.
+    Returns:
+        The functions result.
+    """
+
+    def decorator(f):
+        """
+        Inner decorator
+        """
+
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            """
+            Function cache
+            """
+
+            key = get_key(f, *args, **kwargs)
+
+            cached_result = get(key, deserialize=deserialize)
+
+            if cached_result is None or no_cache:
+                function_result = f(*args, **kwargs)
+
+                set(key, function_result, timeout=timeout, serialize=serialize)
                 return function_result
 
             return cached_result
