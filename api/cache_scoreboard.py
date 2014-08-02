fdb3d19dd88371300bc5d8263ddf02f28ba8b96c
Tim Becker <tjbecker@cmu.edu>
2014-08-02 13:20:57 -0400

    Beginnig to organize the stats caching script better

--- api/cache_scoreboard.py
+++ api/cache_scoreboard.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+
 import api
 import time
 
-while True:
+def cache_team_scores():
+    """
+    A hacky looking way to evaluate and cache the call to get_all_team_scores.
+    """
+
     f = api.stats.get_all_team_scores
     result = f(cache=False)
     key = api.cache.get_mongo_key(f)
     api.cache.set(key, result)
+
+while True:
+    cache_team_scores()
     time.sleep(60)
