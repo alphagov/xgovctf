a6657ee96cfb6b04990ab56d11ed24e3b3b4057b
Tim Becker <tjbecker@cmu.edu>
2014-08-04 10:51:08 -0400

    keyword args matter

--- api/api/stats.py
+++ api/api/stats.py
@@ -15,7 +15,7 @@ class EST(tzinfo):
     def dst(self, dt):
         return datetime.timedelta(0)
 
-end = datetime(2014, 11, 7, 23, 59, 59, EST())
+end = datetime(2014, 11, 7, 23, 59, 59, tzinfo=EST())
 
 @api.cache.memoize()
 def get_score(tid=None, uid=None):
