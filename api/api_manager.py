9fd38edb1f676e710502a170627d147e745f9390
Jonathan Burket <jburket@cmu.edu>
2014-10-14 20:13:18 -0400

    Beginning work on updated problem loading

--- api/api_manager.py
+++ api/api_manager.py
@@ -5,16 +5,21 @@ Problem management script.
 
 import argparse
 import sys
-import imp
-import random
-import shutil
-import os
-
+import logging
 import api
 from api.common import APIException, InternalException
 
 from os import path
 from bson import json_util
+
+
+def check_files_exist(files):
+    for f in files:
+        if not path.isfile(f):
+            logging.critical("No such file {}".format(f))
+            return False
+    return True
+
 
 def insert_objects(f, files):
     objects = get_json_objects(files)
@@ -25,15 +30,27 @@ def insert_objects(f, files):
             raise
             exit(1)
 
+
 def get_json_objects(files):
     objects = []
-    for contents in files.values():
-        for line in contents:
-            obj = json_util.loads(line.strip())
-            objects.append(obj)
+    for f in files:
+        contents = open(f, "r").read()
+        data = json_util.loads(contents)
+        if isinstance(data, list):
+            objects += data
+        elif isinstance(data, dict):
+            objects.append(data)
+        else:
+            logging.warning("JSON file {} did not contain an object or list of objects".format(f))
     return objects
 
-def migrate_problems(files, output_file, debug):
+
+def migrate_problems(args):
+    files = args.files
+    output_file = args.output
+
+    if not check_files_exist(files):
+        return
 
     migration_key = {
         "displayname": "name",
@@ -82,22 +99,27 @@ def migrate_problems(files, output_file, debug):
         output += json_util.dumps(problem) + "\n"
     output_file.write(output)
 
-def build_autogen(instances):
+
+def build_autogen(args):
+    instances = args.instance_count
     problems = api.problem.get_all_problems(show_disabled=True)
     for problem in problems:
         if problem.get("autogen", False):
             api.autogen.build_problem_instances(problem["pid"], instances)
 
-def list_problems():
+
+def list_problems(args):
     #TODO: This could be improved
     problems = api.problem.get_all_problems(show_disabled=True)
     for problem in problems:
         print("{} ({}) - {} points".format(problem["name"], "disabled" if problem["disabled"] else "enabled", problem["score"]))
 
+
 def drop_collections(collections):
     db = api.common.get_conn()
     for collection in collections:
         db[collection].remove()
+
 
 def get_output_file(output):
     if output == sys.stdout:
@@ -106,62 +128,72 @@ def get_output_file(output):
         try:
             return open(output, "w")
         except IOError as error:
-            print(error)
+            logging.warning(error)
             exit(1)
+
+
+def add_new_problems(args):
+    if check_files_exist(args.files):
+        insert_objects(api.problem.insert_problem, args.files)
+        errors = api.problem.analyze_problems()
+        for error in errors:
+            logging.warning(error)
+
+
+def add_new_achievements(args):
+    if check_files_exist(args.files):
+        insert_objects(api.achievement.insert_achievement, args.files)
+
 
 def main():
     parser = argparse.ArgumentParser(description="{} problem manager".format(api.config.competition_name))
+    debug_level = parser.add_mutually_exclusive_group()
+    debug_level.add_argument('-v', '--verbose', help="Print intermediate results", action="store_true")
+    debug_level.add_argument('-s', '--silent', help="Print out very little", action="store_true")
+    subparser = parser.add_subparsers(help='Select one of the following actions')
 
+    # Autogen
+    parser_autogen = subparser.add_parser('autogen', help='Deal with Problem Autogeneration')
+    subparser_autogen = parser_autogen.add_subparsers(help='Select one of the following actions')
 
-    parser.add_argument("-l", action="store_true", dest="show_list", help="View problem list")
-    #TODO: Implement this?
-    #parser.add_argument("-f", action="append", default=[], dest="filters", help="Key:value pairs that are used to search the database for problems. Used in conjuction with -l.")
-    parser.add_argument("-m", action="store_true", dest="migrate", help="Migrate old 2013 problems to new format.", default=False)
-    parser.add_argument("-b", "--build-autogen", action="store", type=int, help="Generate a specified amount of instances for a given list of problems.", default=0)
-    parser.add_argument("-d", action="store_true", dest="debug", help="Debug mode", default=False)
-    parser.add_argument("-o", action="store", dest="output_file", help="Output file.", default=sys.stdout)
-    parser.add_argument("--no-confirm", action="store_true", dest="no_confirm", help="Remove confirmation and assume default action.")
-    parser.add_argument("-a", action="store_true", dest="achievements", help="Assume files contain achievements.")
-    parser.add_argument("-c", "--drop-collections", dest="collections_string", action="store", help="Remove all entries from the listed collections in the database.")
+    parser_autogen_build = subparser_autogen.add_parser('build', help='Build new autogen instances')
+    parser_autogen_build.add_argument("instance_count", nargs=1, help="How many instances of each problem to build")
+    parser_autogen_build.set_defaults(func=build_autogen)
 
-    parser.add_argument("files", nargs="*", help="Files containing problems to insert.")
+    # Problems
+    parser_problems = subparser.add_parser('problems', help='Deal with Problems')
+    subparser_problems = parser_problems.add_subparsers(help='Select one of the following actions')
+
+    parser_problems_load = subparser_problems.add_parser('load', help='Load problems into the database')
+    parser_problems_load.add_argument("files", nargs="+", help="Files containing problems to insert.")
+    parser_problems_load.set_defaults(func=add_new_problems)
+
+    parser_problems_list = subparser_problems.add_parser('list', help='List problems in the database')
+    parser_problems_list.set_defaults(func=list_problems)
+
+    parser_problems_migrate = subparser_problems.add_parser('migrate', help='Migrate 2013 problems to the new format')
+    parser_problems_migrate.add_argument('-o', '--output', action="store", help="Output file.", default=sys.stdout)
+    parser_problems_migrate.set_defaults(func=migrate_problems)
+
+    # Achievements
+    parser_achievements = subparser.add_parser('achievements', help='Deal with Achievements')
+    subparser_achievements = parser_achievements.add_subparsers(help='Select one of the following actions')
+
+    parser_achievements_load = subparser_achievements.add_parser('load', help='Load new achievements into the database')
+    parser_achievements_load.add_argument("files", nargs="+", help="Files containing achievements to insert.")
+    parser_achievements_load.set_defaults(func=add_new_achievements)
 
     args = parser.parse_args()
-
-    files = {}
-
-    if args.collections_string:
-        collections = map(lambda s: s.strip(), args.collections_string.split(","))
-        drop_collections(collections)
-        exit(0)
-    if args.build_autogen > 0:
-        build_autogen(args.build_autogen)
-        exit(0)
-    if args.show_list:
-        list_problems()
-        exit(0)
-
-    #Check that all listed files exist.
-    if len(args.files) == 0:
-        parser.print_help()
-        exit(1)
-
-    for file_path in args.files:
-        if not path.isfile(file_path):
-            print("{}: File does not exist!".format(file_path))
-            exit(1)
-        with open(file_path, "r") as f:
-            files[file_path] = f.readlines()
-
-    if args.migrate:
-        output_file = get_output_file(args.output_file)
-        migrate_problems(files, output_file, args.debug)
+    if args.silent:
+        logging.basicConfig(level=logging.CRITICAL, stream=sys.stdout)
+    elif args.verbose:
+        logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
     else:
-        if args.achievements:
-            insert_objects(api.achievement.insert_achievement, files)
-        else:
-            insert_objects(api.problem.insert_problem, files)
-            errors = api.problem.analyze_problems()
-            for error in errors:
-                print(error)
+        logging.basicConfig(level=logging.WARNING, stream=sys.stdout)
+
+    if 'func' in args:
+        args.func(args)
+    else:
+        parser.print_help()
+
 main()
