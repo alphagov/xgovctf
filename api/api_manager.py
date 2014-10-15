bf90714d43c5dd935c77a39af203a890c67287cb
Jonathan Burket <jburket@cmu.edu>
2014-10-14 21:46:23 -0400

    Updates to problem loading code

--- api/api_manager.py
+++ api/api_manager.py
@@ -8,9 +8,10 @@ import sys
 import logging
 import api
 from api.common import APIException, InternalException
-
-from os import path
+from os import path, walk, makedirs
 from bson import json_util
+import shutil
+import glob
 
 
 def check_files_exist(files):
@@ -145,6 +146,65 @@ def add_new_achievements(args):
         insert_objects(api.achievement.insert_achievement, args.files)
 
 
+def load_problems(args):
+    problem_dir = args.problems_directory[0]
+    grader_dir = args.graders_directory[0]
+    static_dir = args.static_directory[0]
+    if not path.exists(problem_dir):
+        logging.critical("No such directory: {}".format(problem_dir))
+    for (dirpath, dirnames, filenames) in walk(problem_dir):
+        if "problem.json" in filenames:
+            json_file = path.join(dirpath, 'problem.json')
+            contents = open(json_file, "r").read()
+
+            try:
+                data = json_util.loads(contents)
+            except ValueError as e:
+                logging.warning("Invalid JSON format in file {filename} ({exception})".format(filename=json_file,
+                                                                                              exception=e))
+                continue
+
+            if not isinstance(data, dict):
+                logging.warning("Invalid JSON format in file {}".format(json_file))
+                continue
+
+            if 'name' not in data:
+                logging.warning("Invalid problem format in file {}".format(json_file))
+                continue
+
+            problem_name = data['name']
+            relative_path = path.relpath(dirpath, problem_dir)
+            logging.info("Found problem '{}'".format(problem_name))
+
+            if 'grader' not in dirnames:
+                logging.warning("Problem '{}' appears to have no grader folder. Skipping...".format(problem_name))
+                continue
+
+            grader_path = path.join(grader_dir, relative_path)
+            if path.exists(grader_path):
+                shutil.rmtree(grader_path)
+
+            shutil.copytree(path.join(dirpath, 'grader'), grader_path)
+            logging.info("Graders updated for problem {}".format(problem_name))
+
+            try:
+                api.problem.insert_problem(data)
+            except api.common.WebException as e:
+                logging.info("Problem '{}' was not added to the database. Reason: {}".format(problem_name, e))
+
+            if 'static' in dirnames:
+                logging.info("Found a static directory for '{}'. Copying...".format(problem_name))
+                static_path = path.join(static_dir, relative_path)
+                if path.exists(static_path):
+                    shutil.rmtree(static_path)
+                shutil.copytree(path.join(dirpath, 'static'), static_path)
+
+
+    errors = api.problem.analyze_problems()
+    for error in errors:
+        logging.warning(error)
+
+
 def main():
     parser = argparse.ArgumentParser(description="{} problem manager".format(api.config.competition_name))
     debug_level = parser.add_mutually_exclusive_group()
@@ -157,16 +217,22 @@ def main():
     subparser_autogen = parser_autogen.add_subparsers(help='Select one of the following actions')
 
     parser_autogen_build = subparser_autogen.add_parser('build', help='Build new autogen instances')
-    parser_autogen_build.add_argument("instance_count", nargs=1, help="How many instances of each problem to build")
+    parser_autogen_build.add_argument("instance_count", type=int, help="How many instances of each problem to build")
     parser_autogen_build.set_defaults(func=build_autogen)
 
     # Problems
     parser_problems = subparser.add_parser('problems', help='Deal with Problems')
     subparser_problems = parser_problems.add_subparsers(help='Select one of the following actions')
 
-    parser_problems_load = subparser_problems.add_parser('load', help='Load problems into the database')
-    parser_problems_load.add_argument("files", nargs="+", help="Files containing problems to insert.")
-    parser_problems_load.set_defaults(func=add_new_problems)
+    parser_problems_import = subparser_problems.add_parser('import', help='Import problems (from JSON) into the database')
+    parser_problems_import.add_argument("files", nargs="+", help="Files containing problems to insert.")
+    parser_problems_import.set_defaults(func=add_new_problems)
+
+    parser_problems_load = subparser_problems.add_parser('load', help='Load problems that follow the special problem format')
+    parser_problems_load.add_argument("problems_directory", nargs=1, help="Directory where problems are located")
+    parser_problems_load.add_argument("graders_directory", nargs=1, help="Directory where graders are stored")
+    parser_problems_load.add_argument("static_directory", nargs=1, help="Directory where static problem content is stored")
+    parser_problems_load.set_defaults(func=load_problems)
 
     parser_problems_list = subparser_problems.add_parser('list', help='List problems in the database')
     parser_problems_list.set_defaults(func=list_problems)
