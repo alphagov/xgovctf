542635616e9f0aeb6acaa26b0520e8e5b6194ace
Jonathan Burket <jburket@cmu.edu>
2014-11-05 13:13:56 -0500

    Fixed eligibility code to handle case where team has no members

--- api/api/team.py
+++ api/api/team.py
@@ -233,19 +233,23 @@ def assign_shell_account(tid=None):
 def determine_eligibility(tid=None):
     db = api.common.get_conn()
     members = [x for x in db.users.find({"tid": tid}) if 'disabled' not in x or not x['disabled']]
-    eligible = True
-    justification = []
-    for member in members:
-        if member['background'] not in set(['student_hs', 'student_ms', 'student_el', 'student_home']):
-            eligible = False
-            justification.append("User %s is not a middle or high school student" % member['username'])
-        if member['country'] != "US":
-            eligible = False
-            justification.append("User %s is not from the United States" % member['username'])
+    if len(members) == 0:
+        eligible = False
+        justification = ["Team has no members"]
+    else:
+        eligible = True
+        justification = []
+        for member in members:
+            if member['background'] not in set(['student_hs', 'student_ms', 'student_el', 'student_home']):
+                eligible = False
+                justification.append("User %s is not a middle or high school student" % member['username'])
+            if member['country'] != "US":
+                eligible = False
+                justification.append("User %s is not from the United States" % member['username'])
     db.teams.update({'tid': tid}, {'$set': {'eligible': eligible, 'justification': justification}})
     return eligible
 
-    
+
 
 def recalculate_all_eligibility():
     for team in get_all_teams(show_ineligible=True):
