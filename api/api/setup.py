c91ceeaf6c0c6a3520eecc0b7891080712e9eec7
Christopher Ganas <ganaschristopher@gmail.com>
2015-02-20 13:33:39 -0500

    Closes #38 Indexes now enforce unique usernames and team names

--- api/api/setup.py
+++ api/api/setup.py
@@ -16,10 +16,15 @@ def index_mongo():
     log.debug("Ensuring mongo is indexed.")
 
     db.users.ensure_index("uid", unique=True, name="unique uid")
+    db.users.ensure_index("username", unique=True, name="unique username")
+
     db.groups.ensure_index("gid", unique=True, name="unique gid")
     db.problems.ensure_index("pid", unique=True, name="unique pid")
+
     db.submissions.ensure_index("tid", name="submission tids")
     db.ssh.ensure_index("tid", unique=True, name="unique ssh tid")
+    db.teams.ensure_index("team_name", unique=True, name="unique team names")
+
     db.cache.ensure_index("expireAt", expireAfterSeconds=0)
     db.cache.ensure_index("kwargs", name="kwargs")
     db.cache.ensure_index("args", name="args")
