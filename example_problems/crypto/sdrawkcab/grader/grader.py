7098ffcd762eb1defa6d0c8dc2c156f12cff8e9d
Jonathan Burket <jburket@cmu.edu>
2015-02-23 12:38:04 -0500

    Added some example problems

---
+++ example_problems/crypto/sdrawkcab/grader/grader.py
@@ -0,0 +1,5 @@
+def grade(arg, key):
+    if "backwards_text_rox" in key:
+        return True, "!seod ti seY"
+    else:
+        return False, "Incorrect"
---
+++ example_problems/misc/columbus/grader/grader.py
@@ -0,0 +1,9 @@
+"""
+Grader file for Columbus problem
+"""
+
+def grade(autogen, key):
+  if '1492' in key:
+    return (True, 'Good work!')
+  else:
+    return (False, 'Nope')
