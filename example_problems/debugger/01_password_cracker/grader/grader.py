3def546063cf9848cca565f883a31a2071c2e9b3
David Stent <david.stent@digital.cabinet-office.gov.uk>
2017-01-24 23:26:27 +0000

    BAU: binary reverese engineering flags forcing debugger
    This commit contains two challenges for finding flags in binary files using both reverse engineering and debugging tools.
    
    example_problems/debugger01_password_cracker - contains windows and linux binaries of a simpler level worth 10 points.
    example_problems/debugger/02_password_cracker - contains a more obfuscated binary and is currently worth 50 points.
    
    Solo: @davidstent

---
+++ example_problems/debugger/01_password_cracker/grader/grader.py
@@ -0,0 +1,5 @@
+def grade(arg, key):
+    if "flag_H34RTBL33D_g0t_m3_1n" in key:
+        return True, "Correct"
+    else:
+        return False, "Incorrect"
---
+++ example_problems/debugger/02_password_cracker/grader/grader.py
@@ -0,0 +1,5 @@
+def grade(arg, key):
+    if "flag_Y0uR_F1l3s_G0t_P0wn3d" in key:
+        return True, "Correct"
+    else:
+        return False, "Incorrect"
