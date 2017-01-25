08b04a8af6233ff52bfccbb4fcfa9d7dccad2158
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2017-01-25 09:51:03 +0000

    Add SQL injection and password brute force problems

---
+++ example_problems/web/brute-force/grader/grader.py
@@ -0,0 +1,7 @@
+import os
+
+def grade(arg, flag):
+    if "flag_OxLbgLmjOMx7cV7ubtGq2JVymxAT3KJ0TxW0de06sgDmzOUfXXpfTQ0DygvaCtL" in flag:
+        return True, "Correct!"
+    else:
+        return False, "Try again."
---
+++ example_problems/web/sql-injection/grader/grader.py
@@ -0,0 +1,7 @@
+import os
+
+def grade(arg, flag):
+    if "flag_E6GQPvPYrqX64As9B8RNTcQwOgBjRbdRvhgQjT5UqSTYcHxQdMw8ykgoy9lcqwA" in flag:
+        return True, "Correct!"
+    else:
+        return False, "Try again."
